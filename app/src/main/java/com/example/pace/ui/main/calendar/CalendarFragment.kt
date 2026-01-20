047d15b1085ea2c63c82270ce4685243a513f190
shinhanseok <sshhss031227@gmail.com>
2026-01-20 15:21:21 +0900

    [Feat] 의존성 추가와 캘린더 프래그먼트 추가, 툴바 로직 수정

--- app/src/main/java/com/example/pace/ui/main/calendar/CalendarFragment.kt
+++ app/src/main/java/com/example/pace/ui/main/calendar/CalendarFragment.kt
@@ -4,8 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.view.children
 import androidx.fragment.app.Fragment
 import com.example.pace.databinding.FragmentCalendarBinding
+import com.kizitonwose.calendar.core.*
+import com.kizitonwose.calendar.view.*
+import java.time.YearMonth
+import java.time.format.TextStyle
+import java.time.temporal.WeekFields
+import java.util.Locale
 
 class CalendarFragment: Fragment() {
     lateinit var binding: FragmentCalendarBinding
@@ -16,5 +24,35 @@ class CalendarFragment: Fragment() {
     ): View? {
         binding = FragmentCalendarBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        class DayViewContainer(view: View) : ViewContainer(view) {
+            val textView = view.findViewById<TextView>(com.example.pace.R.id.calendarDayText)
+        }
+        // 캘린더 화면에 띄우기
+        binding.calendarView.dayBinder = object : MonthDayBinder<DayViewContainer> {
+            override fun create(view: View) = DayViewContainer(view)
+            override fun bind(container: DayViewContainer, day: CalendarDay) {
+                container.textView.text = day.date.dayOfMonth.toString()
+            }
+        }
+
+        val currentMonth = YearMonth.now()
+        val firstMonth = currentMonth.minusMonths(10)
+        val lastMonth = currentMonth.plusMonths(10)
+        val firstDayOfWeek = WeekFields.of(Locale.getDefault()).firstDayOfWeek
+        binding.calendarView.setup(firstMonth, lastMonth, firstDayOfWeek)
+        binding.calendarView.scrollToMonth(currentMonth)
+
+
+        // 위에 헤더에다가 각 요일 표시하기
+        val daysOfWeek = daysOfWeek(firstDayOfWeek)
+        binding.legendLayout.root.children.forEachIndexed { index, view ->
+            (view as TextView).text = daysOfWeek[index].getDisplayName(TextStyle.SHORT, Locale.getDefault())
+        }
+
     }
 }
