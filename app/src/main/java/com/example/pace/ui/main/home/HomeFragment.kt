a1ae0c791d7de6ae06ae74f8783a8542db63fc2c
Daeun Lee <dev.leedaeun@gmail.com>
2026-01-22 15:04:26 +0900

    [feat]HomeFragment (#22)

--- app/src/main/java/com/example/pace/ui/main/home/HomeFragment.kt
+++ app/src/main/java/com/example/pace/ui/main/home/HomeFragment.kt
@@ -1,11 +1,20 @@
 package com.example.pace.ui.main.home
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.recyclerview.widget.PagerSnapHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.example.pace.databinding.FragmentHomeBinding
+import com.example.pace.ui.add_schedule.AddScheduleActivity
+import java.time.LocalDate
+import java.util.Locale
 
 class HomeFragment: Fragment() {
     lateinit var binding: FragmentHomeBinding
@@ -15,6 +24,51 @@ class HomeFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        // 일정 추가
+        binding.homeAddScheduleIv.setOnClickListener {
+            startActivity(Intent(context, AddScheduleActivity::class.java))
+        }
+
+        // 하단 캘린더
+        val today = LocalDate.now()
+        val horizontalCalendarAdapter = HorizontalCalendarRVAdapter(today)
+        binding.homeHorizontalCalendarRv.adapter = horizontalCalendarAdapter
+
+        val snapHelper = LinearSnapHelper()
+        snapHelper.attachToRecyclerView(binding.homeHorizontalCalendarRv)
+        binding.homeHorizontalCalendarRv.post{
+            val layoutManager = binding.homeHorizontalCalendarRv.layoutManager as LinearLayoutManager
+            val screenWidth = binding.homeHorizontalCalendarRv.width
+            val itemWidth = screenWidth / 7
+            val offset = (screenWidth / 2) - (itemWidth / 2)
+            val centerPos = Int.MAX_VALUE / 2
+            layoutManager.scrollToPositionWithOffset(centerPos, offset)
+            horizontalCalendarAdapter.changeSelectedDate(centerPos)
+        }
+        binding.homeHorizontalCalendarRv.addOnScrollListener(object :
+            RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    val todayPos = Int.MAX_VALUE / 2
+                    val centerView = snapHelper.findSnapView(recyclerView.layoutManager)
+                    if (centerView != null) {
+                        val position = recyclerView.getChildAdapterPosition(centerView)
+                        horizontalCalendarAdapter.changeSelectedDate(position)
+                        val centerDate = if(position > todayPos){
+                            today.plusDays((position - todayPos).toLong())
+                        } else{
+                            val diff = (todayPos - position).toLong()
+                            today.minusDays(diff)
+                        }
+                        Log.d("Date", centerDate.dayOfMonth.toString())
+                        // Todo: centerDate에 적힌 일정 가져오기
+                    }
+                }
+            }
+        })
+
         return binding.root
     }
 }
