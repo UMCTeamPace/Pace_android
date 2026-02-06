04b698d3231e18dafc1dd5d64a4de1b8a7ddec6b
Daeun Lee <dev.leedaeun@gmail.com>
2026-02-06 18:18:44 +0900

    [feat] 상세 경로 UI 데이터 바인딩 (#62)
    * [feat]경로 상세 조회 UI
    
    * [feat]디자인 구현 (#55)
    
    * [Feat] 경로 일정 (#56)
    
    * [Feat]: 경로 일정 연결
    
    * [Feat]: 지도 상세 틀 만들기
    
    * [feat] hilt + retrofit 작성
    
    * [Feat] 경로 선택시 일정 화면에 데이터 연동 구현 (#58)
    
    * [Feat]: 경로일정 경로 데이터 전달
    
    * [Docs]: 주석 수정
    
    * [Fix]: impl대소문자와 폴더구조 수정 (#59)
    
    * [Refactor]: 코드 구조 리팩토링 및 좀 더 부드러운 애니메이션
    
    * [Fix]: 대소문자 폴더 오류 수정
    
    * [Refactor] searchDB 리펙토링, 경로 저장 필드타입 변경 (#60)
    
    * [Refactor]: searchDb 리펙토링
    
    * [Refactor]: 경로 필터 데이터 저장 데이터 타입 수정
    
    * [Feat]: 최근 경로 룸db 초기 구현 (#61)
    
    * [feat]상세 경로 UI 데이터 바인딩
    
    ---------
    
    Co-authored-by: yongyong213 <146186141+yongyong213@users.noreply.github.com>
    Co-authored-by: sungahbak <pasunga12@gmail.com>
    Co-authored-by: Shin hanseok <113006166+0Whitebird0@users.noreply.github.com>

--- app/src/main/java/com/example/pace/ui/main/route/RouteFragment.kt
+++ app/src/main/java/com/example/pace/ui/main/route/RouteFragment.kt
@@ -392,7 +392,7 @@ class RouteFragment : Fragment() {
             behavior.state = BottomSheetBehavior.STATE_COLLAPSED
             behavior.peekHeight = (250 * resources.displayMetrics.density).toInt() // 지도 보일 정도 높이
 
-            RouteDetailHelper.setupData(bottomSheetView, item)
+            RouteDetailHelper.setupData(requireContext(),bottomSheetView, item, selectedEndPlace?.first ?: "")
 
 
         }
