4eab14976d910ff8ca0b44b64153e7cab94487e6
Daeun Lee <dev.leedaeun@gmail.com>
2026-02-16 21:54:04 +0900

    [feat]캘린더 페이지 일정 바인딩

--- app/src/main/java/com/example/pace/ui/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/example/pace/ui/onboarding/OnboardingActivity.kt
@@ -1,4 +1,32 @@
 package com.example.pace.ui.onboarding
 
-class OnBoardingActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.view.animation.DecelerateInterpolator
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.pace.R
+import com.example.pace.data.model.OnboardingItem
+import com.example.pace.databinding.ActivityOnboardingBinding
+
+class OnboardingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOnboardingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 액티비티가 처음 생성될 때만 OnboardingFragment를 붙여줍니다.
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, OnboardingFragment())
+                .commit()
+        }
+    }
+    fun moveToPermissionStep() {
+        val intent = Intent(this, PermissionActivity::class.java)
+        startActivity(intent)
+        finish() // 온보딩 액티비티 종료
+    }
 }
