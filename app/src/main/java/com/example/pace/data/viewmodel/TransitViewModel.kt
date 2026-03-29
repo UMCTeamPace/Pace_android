627071ee94e14a006f9a94267a485ea352e485b4
Daeun Lee <dev.leedaeun@gmail.com>
2026-03-29 16:07:25 +0900

    [feat]지하철 시간표 구현

--- app/src/main/java/com/example/pace/data/viewmodel/TransitViewModel.kt
+++ app/src/main/java/com/example/pace/data/viewmodel/TransitViewModel.kt
@@ -7,30 +7,42 @@ import com.example.pace.BuildConfig
 import com.example.pace.data.api.RetrofitClient
 import com.example.pace.data.datasource.AuthDataStore
 import com.example.pace.data.model.response.BusItemList
-import com.example.pace.data.model.response.BusTransitResponse
-//import com.example.pace.data.model.response.SubwayTimeTableResponse
-import com.example.pace.data.model.response.SubwayTimetableItem
-import com.example.pace.data.model.response.SubwayTransitResponse
+import com.example.pace.data.model.response.StationTimetableItem
 import com.example.pace.data.model.response.SubwayTransitResult
 import com.google.gson.Gson
-import com.google.gson.JsonDeserializationContext
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 
 @HiltViewModel
 class TransitViewModel @Inject constructor(
     private val authStore: AuthDataStore
 ) : ViewModel() {
+    // 실시간 지하철
     private val _subwayResult = MutableStateFlow<List<SubwayTransitResult>>(emptyList())
-    val subwayResult = _subwayResult.value
+    val subwayResult = _subwayResult.asStateFlow()
 
-    private val _subwayTimetable = MutableStateFlow<List<SubwayTimetableItem>>(emptyList())
-    val subwayTimetable = _subwayTimetable.value
+    // 지하철 시간표
+    private val _firstUpSubway = MutableStateFlow<List<StationTimetableItem?>>(emptyList())
+    val firstUpSubway = _firstUpSubway.asStateFlow()
+    private val _firstDownSubway = MutableStateFlow<List<StationTimetableItem?>>(emptyList())
+    val firstDownSubway = _firstDownSubway.asStateFlow()
+    private val _lastUpSubway = MutableStateFlow<List<StationTimetableItem?>>(emptyList())
+    val lastUpSubway = _lastUpSubway.asStateFlow()
+    private val _lastDownSubway = MutableStateFlow<List<StationTimetableItem?>>(emptyList())
+    val lastDownSubway = _lastDownSubway.asStateFlow()
+    // 지하철 종점역들
+    private val _endStations = MutableStateFlow<List<String>>(emptyList())
+    val endStations = _endStations.asStateFlow()
 
+    // 실시간 버스 정보
     private val _busResult = MutableStateFlow<BusItemList?>(null)
-    val busResult = _busResult.value
+    val busResult = _busResult.asStateFlow()
 
     // 실시간 지하철 도착 정보
     fun getRealTimeSubwayArrivals(startStationName: String, endStationName: String, lineName: String){
@@ -57,25 +69,169 @@ class TransitViewModel @Inject constructor(
     }
 
     // 지하철 시간표
-    fun getSubwayTimetable2(isTempOrNot: String, upbdnSe: String, wkndSe: String, lineNm: String, stnNm: String,){
+    fun getSubwayFirstAndLast(station: String, lineName: String, dailyType: String, upDown: String){
         viewModelScope.launch {
-            try{
-                val response = RetrofitClient.subwayTimetableService.getSubwayTimetable2(BuildConfig.PUBLIC_API_KEY, "200", "JSON", isTempOrNot, upbdnSe, wkndSe, lineNm, stnNm)
-                val value = response.values.firstOrNull()
-                val items = value?.body?.items?.item
-                if(!items.isNullOrEmpty()){
-                    // 지하철 도착 순서가 이른 순서대로 정렬
-                    val sortedItems = items.sortedBy {
-                        item -> item.trainArvlTm
+            try {
+                // 지하철역 아이디 검색
+                val idResponse = RetrofitClient.subwayTimetableService.getSubwayStationId(BuildConfig.PUBLIC_API_KEY, "1", "30", "json", station)
+                val idItems = idResponse.values.firstOrNull()?.body?.items?.item
+                var stationId = ""
+
+                // 호선명 패러미터에 맞게 변환
+                val mLineName = when (lineName) {
+                    "인천 1호선", "인천 2호선" -> {
+                        lineName.replace(" ","")
                     }
-                    _subwayTimetable.value = sortedItems
-                    Log.d("TRANSIT_SUCCESS", _subwayTimetable.value.toString())
-                }else{
-                    Log.d("TRANSIT_ERROR", "item is null")
+                    "경의중앙선", "경춘선", "우이신설선" -> {
+                        lineName.dropLast(1)
+                    }
+                    "수인.분당선" -> "수인분당"
+                    "공항철도" -> "공항"
+                    else -> {
+                        lineName
+                    }
                 }
-            }
-            catch (e: Exception){
-                Log.e("TRANSIT_ERROR", e.message.toString())
+
+                idItems?.forEach {
+                    // () 안 내용 및 공백 제거
+                    if(station == it.stationName.split("(").first() && mLineName == it.routeName){
+                        stationId = it.stationId
+                    }
+                }
+
+                // 지하철 시간표 검색
+                val formatter = DateTimeFormatter.ofPattern("HHmmss")
+                val first = mutableListOf<StationTimetableItem?>()
+                val last = mutableListOf<StationTimetableItem?>()
+
+                val timetableResponse =
+                    RetrofitClient.subwayTimetableService.getSubwayTimetable(
+                        BuildConfig.PUBLIC_API_KEY,
+                        "1",
+                        "240",
+                        "json",
+                        stationId,
+                        dailyType,
+                        upDown
+                    )
+                val timetableItems = timetableResponse.values.firstOrNull()?.body?.items?.item
+                if (!timetableItems.isNullOrEmpty()) {
+                    // 종착역 별로 첫차 및 막차 나누기
+                    val itemPair = mutableMapOf<String, Pair<List<StationTimetableItem>, List<StationTimetableItem>>?>()
+                    val groupedItems = timetableItems.groupBy { it.endStationId }
+                    for (endStation in groupedItems.keys) {
+                        if(endStation == null || endStation == stationId){
+                            continue
+                        }
+                        // Map<종착역, Pair<막차, 첫차>>
+                        itemPair[endStation] = groupedItems[endStation]?.partition {
+                            if(it.depTime == "0"){
+                                LocalTime.parse(it.arrTime, formatter)
+                                    .isBefore(LocalTime.parse("05:00:00"))
+                                        || LocalTime.parse(it.arrTime, formatter)
+                                    .isAfter(LocalTime.parse("21:00:00"))
+                            }else{
+                                LocalTime.parse(it.depTime, formatter)
+                                    .isBefore(LocalTime.parse("05:00:00"))
+                                        || LocalTime.parse(it.depTime, formatter)
+                                    .isAfter(LocalTime.parse("21:00:00"))
+                            }
+
+                        }
+
+                        // 막차(01시 기준으로 시간 나누기)
+                        val lastDepTimes = itemPair[endStation]?.first?.map {
+                            if(it.depTime == "0"){
+                                LocalTime.parse(it.arrTime, formatter)
+                            }else{
+                                LocalTime.parse(it.depTime, formatter)
+                            }
+                        }
+                        var filteredLastDepTimes = lastDepTimes
+                        if(lastDepTimes?.find{
+                            it.isBefore(LocalTime.parse("01:00:00"))
+                        } != null){
+                            filteredLastDepTimes = lastDepTimes.filter{
+                                it.isBefore(LocalTime.parse("01:00:00"))
+                            }
+                        }
+                        val latest = filteredLastDepTimes?.stream()?.max(LocalTime::compareTo)?.orElse(null)
+                        last.add(itemPair[endStation]?.first?.find {
+                            if(it.depTime == "0"){
+                                LocalTime.parse(it.arrTime, formatter) == latest
+                            }else{
+                                LocalTime.parse(it.depTime, formatter) == latest
+                            }
+                        })
+
+                        // 첫차
+                        val firstDepTimes = itemPair[endStation]?.second?.map {
+                            if(it.depTime == "0"){
+                                LocalTime.parse(it.arrTime, formatter)
+                            }else{
+                                LocalTime.parse(it.depTime, formatter)
+                            }
+                        }
+                        val earliest = firstDepTimes?.stream()?.min(LocalTime::compareTo)?.orElse(null)
+                        first.add(itemPair[endStation]?.second?.find {
+                            if(it.depTime == "0"){
+                                LocalTime.parse(it.arrTime, formatter) == earliest
+                            }else{
+                                LocalTime.parse(it.depTime, formatter) == earliest
+                            }
+                        })
+                    }
+
+                    // 첫차 및 막차 시간 순으로 정렬
+                    val sortedFirst = first.filterNotNull().sortedBy{
+                        if(it.depTime == "0"){
+                            LocalTime.parse(it.arrTime, formatter)
+                        }else{
+                            LocalTime.parse(it.depTime, formatter)
+                        }
+                    }
+                    val sortedLast = last.filterNotNull()
+                        .sortedWith(
+                        compareBy<StationTimetableItem?>{
+                            if(it?.depTime == "0"){
+                                !LocalTime.parse(it.arrTime, formatter).isAfter(LocalTime.parse("21:00:00"))
+                            }else{
+                                !LocalTime.parse(it?.depTime, formatter).isAfter(LocalTime.parse("21:00:00"))
+                            }
+                        }.thenBy {
+                            if(it?.depTime == "0"){
+                                LocalTime.parse(it.arrTime, formatter)
+                            }else{
+                                LocalTime.parse(it?.depTime, formatter)
+                            }
+                        }
+                    )
+
+                    // todo: 종점 역 저장 변수 사용하기
+                    _endStations.value = timetableItems.mapNotNull {
+                        it.endSubwayStationName
+                    }.distinct()
+                    // 상하행 여부에 따라 맞게 저장
+                    when(upDown){
+                        "U" -> {
+                            _firstUpSubway.value = sortedFirst
+                            _lastUpSubway.value = sortedLast
+                            Log.d("TRANSIT_SUCCESS: first/up", _firstUpSubway.value.toString())
+                            Log.d("TRANSIT_SUCCESS: last/up", _firstUpSubway.value.toString())
+                        }
+                        "D" -> {
+                            _firstDownSubway.value = sortedFirst
+                            _lastDownSubway.value = sortedLast
+                            Log.d("TRANSIT_SUCCESS: first/down", _firstDownSubway.value.toString())
+                            Log.d("TRANSIT_SUCCESS: last/down", _lastDownSubway.value.toString())
+                        }
+                    }
+                } else {
+                    Log.e("TRANSIT_ERROR", "Item is null")
+                }
+            }catch (e: HttpException){
+                val errorJson = e.response()?.errorBody()?.string()
+                Log.e("TRANSIT_ERROR", errorJson!!)
             }
         }
     }
@@ -101,7 +257,7 @@ class TransitViewModel @Inject constructor(
 
                     Log.d("TRANSIT_SUCCESS", _busResult.value.toString())
                 } else{
-                    Log.e("TRANSIT_ERROR", "result is null")
+                    Log.e("TRANSIT_ERROR", "Result is null")
                 }
             } catch (e:Exception){
                 Log.e("TRANSIT_ERROR", e.message.toString())
